1d63e74e93e2cc793aae275b36d5686cf2ddb015
Aryan Satyendra Kumar <kumararyan66472@gmail.com>
2026-05-31 13:30:25 +0530

    Removed the secrets

--- src/firebase.js
+++ src/firebase.js
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDwffsBWXE9Gfj8gNLMYyjLdjzyX_8PgUY",
-  authDomain: "roadsos-e95f5.firebaseapp.com",
-  projectId: "roadsos-e95f5",
-  storageBucket: "roadsos-e95f5.firebasestorage.app",
-  messagingSenderId: "891087478163",
-  appId: "1:891087478163:web:48b25eea1255d22c3a9c66"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID
 };
 
 const app = initializeApp(firebaseConfig);
